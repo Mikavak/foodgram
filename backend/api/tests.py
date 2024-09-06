3150c7506db322241349033a8ac011dee78a8d90
Mik <awvvak@gmail.com>
2024-09-06 19:58:44 +0300

    Add action

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,18 +1,11 @@
-from http import HTTPStatus
-
-from django.contrib.auth import get_user_model
 from django.test import TestCase
-from rest_framework.test import APIClient
 
 
-class CatsAPITestCase(TestCase):
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(username='auth_user')
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
+class TestRoutes(TestCase):
 
-    def test_list_exists(self):
-        """Проверка доступности списка задач."""
-        response = self.client.get('/api/recipes/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_home_page(self):
+        # Вызываем метод get для клиента (self.client)
+        # и загружаем главную страницу.
+        response = self.client.get('/')
+        # Проверяем, что код ответа равен 200.
+        self.assertEqual(response.status_code, 200)
